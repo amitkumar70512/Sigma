6e691ba6e9e659d6aa357185fd8266b5d189cbd5
amit kumar <amitkumar.cs19@bmsce.ac.in>
2023-12-02 17:47:18 +0530

    code refactoring

--- src/components/NoPage/NoPage.tsx
+++ src/components/NoPage/NoPage.tsx
@@ -1,5 +1,21 @@
+import { animated, useSpring } from '@react-spring/web'
+import './NoPage.css';
+
 const NoPage = () => {
-    return <h1>404</h1>;
+    const props = useSpring({
+        opacity: 1,
+        from: { opacity: 0 },
+        reset: true,
+    });
+
+    return (
+        <animated.div style={props}>
+            <div className="not-found-container">
+                <h1>404 - Not Found</h1>
+                <p>Sorry, the page you are looking for might be in another castle.</p>
+            </div>
+        </animated.div>
+    );
 };
 
 export default NoPage;
