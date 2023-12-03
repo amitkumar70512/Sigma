cecfc7a9dd5b7e1192d4fe2c32b0a2051d7427dd
amit kumar <amitkumar.cs19@bmsce.ac.in>
2023-12-03 15:01:42 +0530

    added border to code

--- src/components/plugins/Spinner/Spinner.tsx
+++ src/components/plugins/Spinner/Spinner.tsx
@@ -1,6 +1,7 @@
 // src/components/Spinner.tsx
 import React, { useState, ChangeEvent } from 'react';
 import styled, { keyframes } from 'styled-components';
+import HtmlCss from '../../Styles/HtmlCss';
 
 interface SpinnerAttributes {
     size: number;
@@ -12,6 +13,14 @@ interface SpinnerAttributes {
 const rotateAnimation = keyframes`
   0% { transform: rotate(0deg); }
   100% { transform: rotate(360deg); }
+`;
+
+const SizeInput = styled.input`
+  width: 20%;
+`;
+
+const SpeedInput = styled.input`
+  width: 20%;
 `;
 
 const SpinnerWrapper = styled.div<SpinnerAttributes>`
@@ -41,50 +50,91 @@ const Spinner: React.FC = () => {
         }));
     };
 
+    // Function to generate HTML and CSS code
+    const generateCode = () => {
+        const { size, color, background, speed } = spinnerAttributes;
+
+        const htmlCode = `
+      <div style="width: ${size}px; height: ${size}px; border: 4px solid ${color}; border-top: 4px solid ${background}; border-radius: 50%; animation: spin ${speed}s linear infinite;"></div>
+    `;
+
+        const cssCode = `
+      @keyframes spin {
+        0% { transform: rotate(0deg); }
+        100% { transform: rotate(360deg); }
+      }
+
+      .spinner {
+        width: ${size}px;
+        height: ${size}px;
+        border: 4px solid ${color};
+        border-top: 4px solid ${background};
+        border-radius: 50%;
+        animation: spin ${speed}s linear infinite;
+      }
+    `;
+
+        return { htmlCode, cssCode };
+    };
+
+    const { htmlCode, cssCode } = generateCode();
+
     return (
-        <div>
+        <div style={{ display: 'flex', width: '100vw' }}>
             <h2>Spinner Creator</h2>
-            <SpinnerWrapper {...spinnerAttributes} />
-            <div>
-                <label>Size:</label>
-                <input
-                    type="number"
-                    value={spinnerAttributes.size}
-                    onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                        updateSpinnerAttributes('size', parseInt(e.target.value, 10))
-                    }
-                />
-            </div>
-            <div>
-                <label>Color:</label>
-                <input
-                    type="color"
-                    value={spinnerAttributes.color}
-                    onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                        updateSpinnerAttributes('color', e.target.value)
-                    }
-                />
-            </div>
-            <div>
-                <label>Background:</label>
-                <input
-                    type="color"
-                    value={spinnerAttributes.background}
-                    onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                        updateSpinnerAttributes('background', e.target.value)
-                    }
-                />
-            </div>
-            <div>
-                <label>Speed:</label>
-                <input
-                    type="number"
-                    step="0.1"
-                    value={spinnerAttributes.speed}
-                    onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                        updateSpinnerAttributes('speed', parseFloat(e.target.value))
-                    }
-                />
+
+            <div className="left" style={{ border: '1px solid #ddd', padding: '10px', borderRadius: '5px', minWidth: '40%', maxWidth: '40%' }}>
+                <SpinnerWrapper {...spinnerAttributes} />
+                <div>
+                    <label>Size:</label>
+                    <SizeInput
+                        type="range"
+                        min={20}
+                        max={150}
+                        value={spinnerAttributes.size}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                            updateSpinnerAttributes('size', parseInt(e.target.value, 10))
+                        }
+                    />
+                </div>
+                <div>
+                    <label>Color:</label>
+                    <input
+                        type="color"
+                        value={spinnerAttributes.color}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                            updateSpinnerAttributes('color', e.target.value)
+                        }
+                    />
+                </div>
+                <div>
+                    <label>Background:</label>
+                    <input
+                        type="color"
+                        value={spinnerAttributes.background}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                            updateSpinnerAttributes('background', e.target.value)
+                        }
+                    />
+                </div>
+                <div>
+                    <label>Speed:</label>
+                    <SpeedInput
+                        type="range"
+                        min={0.1}
+                        max={5}
+                        step={0.1}
+                        value={spinnerAttributes.speed}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+                            updateSpinnerAttributes('speed', parseFloat(e.target.value))
+                        }
+                    />
+                </div>
+            </div>,
+            <div className='right' style={{ display: 'flex', flexFlow: 'column', minWidth: '60%' }}>
+                <h3>Generated Code</h3>
+                <pre  ><HtmlCss /> {htmlCode}</pre>
+                <pre style={{ border: '1px solid #ddd', padding: '10px', borderRadius: '5px' }}>{cssCode}</pre>
             </div>
         </div>
     );
